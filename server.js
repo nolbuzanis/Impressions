9b3893e562f469b1b5847d0ce6d699a234443b9b
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-04 17:20:26 -0400

    Ready for heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const PORT = process.env.PORT | 8000;
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
