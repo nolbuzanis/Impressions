ada2d194b75b14db99f6762276b6a40173f1a863
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-04 21:41:38 -0400

    Connected Spotify OAuth with passport js and spotify developer dashboard.

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Spotify OAuth
+
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: keys.spotifyClientID,
+      clientSecret: keys.spotifyClientSecret,
+      callbackURL: '/auth/spotify/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/spotify',
+  passport.authenticate('spotify', {
+    scope: ['user-library-read', 'user-read-email', 'user-read-private']
+  })
+);
 
 const PORT = process.env.PORT | 8000;
 app.listen(PORT, () => {
