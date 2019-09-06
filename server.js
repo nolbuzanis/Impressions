f0000f3c41bc62e37f66a88b7c87baca07bbbf95
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-06 17:51:22 -0400

    Refactor before adding mongoDB

--- server.js
+++ server.js
@@ -1,40 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const SpotifyStrategy = require('passport-spotify').Strategy;
-const keys = require('./config/keys');
-
 const app = express();
 
-// Spotify OAuth
-
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: keys.spotifyClientID,
-      clientSecret: keys.spotifyClientSecret,
-      callbackURL: '/auth/spotify/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/spotify',
-  passport.authenticate('spotify', {
-    scope: ['user-library-read', 'user-read-email', 'user-read-private']
-  })
-);
-
-app.get(
-  '/auth/spotify/callback',
-  passport.authenticate('spotify', {
-    failureRedirect: '/'
-  })
-);
+require('./routes/authRoutes')(app); // Spotify OAuth
+require('./services/passport'); //Passport config
 
 const PORT = process.env.PORT | 8000;
 app.listen(PORT, () => {
---
+++ services/passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const keys = require('../config/keys');
+
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: keys.spotifyClientID,
+      clientSecret: keys.spotifyClientSecret,
+      callbackURL: '/auth/spotify/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+    }
+  )
+);
