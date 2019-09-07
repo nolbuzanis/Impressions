6d77d68446543f634498cfbb5dfe5f2f9ba77020
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-07 13:44:59 -0400

    Installed cookie-session to make express work with cookies. Set up passport to use cookies for authentication too

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const keys = require('./config/keys');
 
 mongoose.connect(keys.mongoURI);
 require('./models/User');
+
+const app = express();
+
+// tell express to make use of cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //Last for 30 days (in microseconds)
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app); // Spotify OAuth
 require('./services/passport'); //Passport config
