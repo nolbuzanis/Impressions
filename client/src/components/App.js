0b54b9769d6d79235bce8f99240f6f430e8fc6ca
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-08 00:33:21 -0400

    Created fetchUser action creator which is called when app loads

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Landing from './Landing';
 import Dashboard from './Dashboard';
 import Header from './Header';
 
-import { BrowserRouter, Route } from 'react-router-dom';
-
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
   render() {
     return (
       <div>
@@ -21,4 +27,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App);
