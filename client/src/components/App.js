c3028bd78c16de1231b8078d37b502f8d6096be5
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-07 21:08:55 -0400

    Setup react browser router, flushed out header spotify profile icon

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
+import Header from './Header';
+
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Landing />
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path='/' component={Landing} exact />
+            <Route path='/profile' component={Dashboard} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return <div>Profile</div>;
+};
+
+export default Dashboard;
