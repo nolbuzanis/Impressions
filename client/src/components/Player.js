d062b71aa7b7ecd8082817758b8b2fd7d35abe21
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-24 13:22:55 -0400

    Designed player with 5 icons - icons pulled from material-design-icons by google

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import './Player.css';
+import { Link } from 'react-router-dom';
 
 class Player extends React.Component {
   render() {
-    return <div className='fixed-bottom player'>Player</div>;
+    return (
+      <ul className='fixed-bottom player'>
+        <li>
+          <Link to='/' className='player-link'>
+            <i class='material-icons'>shuffle</i>
+          </Link>
+        </li>
+        <li>
+          <Link to='/' className='player-link'>
+            <i class='material-icons'>skip_previous</i>
+          </Link>
+        </li>
+        <li>
+          <Link to='/' className='player-link'>
+            <i class='material-icons'>play_arrow</i>
+          </Link>
+        </li>
+        <li>
+          <Link to='/' className='player-link'>
+            <i class='material-icons'>skip_next</i>
+          </Link>
+        </li>
+        <li>
+          <Link to='/' className='player-link'>
+            <i class='material-icons'>repeat</i>
+          </Link>
+        </li>
+      </ul>
+    );
   }
 }
 
