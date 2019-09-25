7a007f91a46abbf26d23183abb4cd2c6aecdd3cd
nolbuzanis <nolanbuzanis@gmail.com>
2019-09-25 15:34:26 -0400

    Fully fixed reload issue with player

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -2,61 +2,8 @@ import React from 'react';
 import SongList from './SongList';
 import Player from './Player';
 import { connect } from 'react-redux';
-import history from './history';
-import { Redirect } from 'react-router-dom';
 
 class Library extends React.Component {
-  componentDidMount() {
-    if (window.loadedSpotifyPlayer) {
-      this.initializePlayer();
-    }
-  }
-
-  initializePlayer = () => {
-    if (window.Spotify !== null) {
-      console.log('Initializing...');
-
-      this.player = new window.Spotify.Player({
-        name: 'Impressions',
-        getOAuthToken: cb => {
-          cb(this.props.auth.accessToken);
-        }
-      });
-
-      // Error handling
-      this.player.on('initialization_error', e => {
-        console.error(e);
-      });
-      this.player.on('authentication_error', e => {
-        console.error(e);
-        return <Redirect to='/dashboard' />;
-      });
-      this.player.on('account_error', e => {
-        console.error(e);
-      });
-      this.player.on('playback_error', e => {
-        console.error(e);
-      });
-
-      // Playback status updates
-      this.player.on('player_state_changed', state => {
-        console.log(state);
-      });
-
-      // Ready
-      this.player.addListener('ready', ({ device_id }) => {
-        console.log('Ready with Device ID', device_id);
-      });
-
-      // Not Ready
-      this.player.addListener('not_ready', ({ device_id }) => {
-        console.log('Device ID has gone offline', device_id);
-      });
-      this.player.connect();
-    }
-    console.log(this.player);
-  };
-
   render() {
     return (
       <div>
--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -2,8 +2,62 @@ import React from 'react';
 import './Player.css';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Player extends React.Component {
+  componentDidMount() {
+    if (window.loadedSpotifyPlayer) {
+      this.initializePlayer();
+    }
+
+    window.addEventListener('load', this.initializePlayer);
+  }
+
+  initializePlayer = () => {
+    if (window.Spotify !== null) {
+      console.log('Initializing...');
+
+      this.player = new window.Spotify.Player({
+        name: 'Impressions',
+        getOAuthToken: cb => {
+          cb(this.props.auth.accessToken);
+        }
+      });
+
+      // Error handling
+      this.player.on('initialization_error', e => {
+        console.error(e);
+      });
+      this.player.on('authentication_error', e => {
+        console.error(e);
+        return <Redirect to='/dashboard' />;
+      });
+      this.player.on('account_error', e => {
+        console.error(e);
+      });
+      this.player.on('playback_error', e => {
+        console.error(e);
+      });
+
+      // Playback status updates
+      this.player.on('player_state_changed', state => {
+        console.log(state);
+      });
+
+      // Ready
+      this.player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+      });
+
+      // Not Ready
+      this.player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+      });
+      this.player.connect();
+    }
+    console.log(this.player);
+  };
+
   render() {
     return (
       <ul className='fixed-bottom player'>
@@ -37,4 +91,11 @@ class Player extends React.Component {
   }
 }
 
-export default connect()(Player);
+const mapStateToProps = state => {
+  return { auth: state.auth };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(Player);
