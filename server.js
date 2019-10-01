585f0975cf5427cffce13c17923692767b571101
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-01 17:14:43 -0400

    Ready for heroku prod env

--- server.js
+++ server.js
@@ -23,11 +23,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
+  // Serve any production assets like our main.css or main.js
+  app.use(express.static('client/build'));
+  // Handle all routing --> serve up index.html if it doesn't recognize the route
+  const path = require('path');
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
