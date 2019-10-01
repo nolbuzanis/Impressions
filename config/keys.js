bf2b9bfc0981ce682ae5e6c9b7815599b42a7ba5
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-01 16:16:30 -0400

    Deleted comments

--- config/keys.js
+++ config/keys.js
@@ -5,7 +5,3 @@ if (process.env.NODE_ENV === 'production') {
   // In development
   module.exports = require('./dev');
 }
-
-//mongodb+srv://dbAdmin:M@rley1997@cluster0-5lvpn.mongodb.net/test?retryWrites=true&w=majority
-// Client ID: c39d9ec26c6d4a4bb5b6dd57182856b1
-// Cient secret: 7c4891a03b3f414d94daa3ee38077dd2
