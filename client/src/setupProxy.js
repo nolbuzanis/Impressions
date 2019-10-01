7ff642ffe4155c2d1d8663d5870d197d22f5125f
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-01 17:54:37 -0400

    Changed back proxy file to before

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = app => {
-  if (process.env.NODE_ENV === 'production') {
-    return null;
-  }
   app.use(
     proxy(['/api', '/auth/spotify'], {
-      target: 'http://localhost:8000'
+      target: 'http://localhost:8000',
+      changeOrigin: true
     })
   );
 };
