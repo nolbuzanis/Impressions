72b8d5a2d306c46771fc7e1f49109b5cdd075559
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-02 08:56:10 -0400

    Refactored app.listen()

--- server.js
+++ server.js
@@ -35,4 +35,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(process.env.PORT | 8000);
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, (req, res) => {
+  console.log('App no listening on port: ', PORT);
+});
